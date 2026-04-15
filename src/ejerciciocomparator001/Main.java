3cab360d0da283b7d2d6f361c97ea846df912c8d
jaime98-coder <jaime.martin@iesnervion.es>
2026-04-15 21:32:58 +0200

    Subida ejercicio

---
+++ src/ejerciciocomparator001/Main.java
@@ -0,0 +1,23 @@
+package ejerciciocomparator001;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		Integer[] listaNumeros = new Integer[20];
+		Random rd = new Random();
+		ComparadorNumerosDecreciente juez = new ComparadorNumerosDecreciente();
+		for (int i = 0; i < listaNumeros.length; i++) {
+			listaNumeros[i] = rd.nextInt(1, 101);
+		}
+		for (int numero : listaNumeros) {
+			System.out.print(numero + "\t");
+		}
+		System.out.println("Ordenado");
+		Arrays.sort(listaNumeros, juez);
+		for (int numero : listaNumeros) {
+			System.out.print(numero + "\t");
+		}
+	}
+}
